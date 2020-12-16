0badef737fad0a06494c3e7b83049996e17f3071
catsnape <71445814+catsnape@users.noreply.github.com>
2020-12-17 00:40:36 +0400

    Add files via upload

---
+++ table area .cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() {
+float edge1;
+float edge2;
+float edge3;
+std::cin >> edge1;
+std::cin >> edge2;
+std::cin >> edge3;
+float c=(edge1+edge2)*edge3/2;
+std::cout  <<c;
+}
