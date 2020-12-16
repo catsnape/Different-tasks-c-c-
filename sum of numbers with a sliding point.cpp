efbf5e72faa57698dd4ab44ac5cce07169e84a99
catsnape <71445814+catsnape@users.noreply.github.com>
2020-12-17 00:43:25 +0400

    Add files via upload

---
+++ sum of numbers with a sliding point.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() 
+{
+  float a;
+  float b;
+  float c;
+  float d;
+  std::cin>>a;
+  std::cin>>b;
+  std::cin>>c;
+  std::cin>>d;
+  
+std::cout <<(a+b)-(c+d);
+}
