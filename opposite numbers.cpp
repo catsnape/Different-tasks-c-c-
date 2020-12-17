29e6eb0d6d2240f168981aedd629d081d30353f8
catsnape <71445814+catsnape@users.noreply.github.com>
2020-12-17 18:19:23 +0400

    Add files via upload

---
+++ opposite numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+ int A;
+ int B;
+ int C;
+ int D;
+ std::cin>>A>>B>>C>>D;
+ if((A==-D)&&(B!=-C))
+ {
+   std::cout<<"YES";
+ }
+ else 
+ {
+   std::cout<<"NO";
+ }
+}
