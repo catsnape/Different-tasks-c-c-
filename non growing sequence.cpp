05a76c7d1be1b096e6d93f24c6bbd18a5771aae8
catsnape <71445814+catsnape@users.noreply.github.com>
2020-12-17 18:16:14 +0400

    Add files via upload

---
+++ non growing sequence.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+  
+  int a;
+  int b;
+  int c;
+  int d;
+  std::cin>>a>>b>>c>>d;
+  if (a>=b && b>=c && c>=d)
+  {
+    std::cout << "YES\n";
+  }
+  else
+  {
+     std::cout<<"NO";
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
