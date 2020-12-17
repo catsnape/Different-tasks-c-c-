8e48730add365ad0cea478ba6ace34ffc2c63280
catsnape <71445814+catsnape@users.noreply.github.com>
2020-12-17 18:13:58 +0400

    Add files via upload

---
+++ belongs to the range.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+  double A;
+  double B;
+  double C;
+   std::cin>>A>>B>>C;
+  if(A>=B && A<=C && B<C && A!=C)
+    {std::cout<<"YES"<<" ";
+    }
+   else 
+  { 
+   std::cout<<"NO"; 
+  }
+   }
+   
+//-1.125 -1.125 -1.1
