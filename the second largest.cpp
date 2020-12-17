4d70f96767640028ad7a8e08fe3f086050057dd6
catsnape <71445814+catsnape@users.noreply.github.com>
2020-12-17 18:07:00 +0400

    Add files via upload

---
+++ the second largest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  int x;
+  std::cin>>x;
+    if ((x%2==0)&& (x%7==3))
+  {
+  
+  std::cout<<"YES"<< std::endl;
+  }
+  else {
+std::cout << "NO" << std::endl;
+}
+}
