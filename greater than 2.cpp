6b0bca681df93c88628f1f5c22317f5833b81976
catsnape <71445814+catsnape@users.noreply.github.com>
2020-12-17 18:03:50 +0400

    Add files via upload

---
+++ greater than 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+long a, b;
+long max;
+std::cin>>a>>b;
+std::cout<<" ";
+  {
+    if (((a >= b) && (max=a))||((b >= a) && (max=b)))
+
+   std::cout  << max;
+
+  }
+}
