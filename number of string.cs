16c013957f16f1e42b8f95df36490311d4324625
catsnape <71445814+catsnape@users.noreply.github.com>
2021-02-24 18:11:23 +0400

    Create number of string.cs

---
+++ number of string.cs
@@ -0,0 +1,17 @@
+  using System;
+  public class MainClass;
+{
+public static void Main()
+  {
+  string s = Console.ReadLine();
+            for (int i = 1; i > 0; i++)
+            {
+                string x = Console.ReadLine();
+                if (x == s)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+    }
+}
