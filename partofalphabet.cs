3f0c68da30388e41cb0febeba8bdfd5d5988e996
catsnape <71445814+catsnape@users.noreply.github.com>
2021-03-09 03:22:13 +0400

    Create partofalphabet.cs
    По заданной строчной букве латинского алфавита, выведите все строчные буквы латинского алфавита, начиная от начала до заданной буквы.

---
+++ partofalphabet.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+       char c=Convert.ToChar(Console.ReadLine());
+       
+       for (char i='a';i<=c;i++)
+          Console.Write(i + " ");
+       
+    }
+}
