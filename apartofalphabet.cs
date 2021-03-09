8efb90b409d79dccb5f90113985226f93c7ad629
catsnape <71445814+catsnape@users.noreply.github.com>
2021-03-10 02:05:22 +0400

    Create apartofalphabet.cs
    По заданной строчной букве латинского алфавита, выведите все строчные буквы латинского алфавита, начиная от заданной буквы до конца алфавита.

---
+++ apartofalphabet.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+       char c =Convert.ToChar(Console.ReadLine());
+        for (char i=c;i<='z';i++)
+        {
+            Console.Write(i + " ");
+         }
+    }
+}
