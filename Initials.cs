577eee330816095842e3201f03d2d77bed0c7e9e
catsnape <71445814+catsnape@users.noreply.github.com>
2021-03-21 01:54:37 +0400

    Create Initials.cs
    По данным ФИО сформируйте строку, содержащую фамилию с инициалами. Например, по ‘Ivanov Ivan Ivanovich’ получите "Ivanov I.I.".

---
+++ Initials.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+       string a = Console.ReadLine();
+       string b = Console.ReadLine();
+       string c = Console.ReadLine();
+        
+        Console.WriteLine(a+ " " +b[0]+ "." +c[0]+ ".");
+    }
+}
