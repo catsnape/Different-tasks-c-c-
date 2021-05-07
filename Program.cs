92444e256fdd8850e1612c620815ea79c1dc84f3
catsnape <71445814+catsnape@users.noreply.github.com>
2021-05-07 23:40:03 +0400

    Add files via upload
    Побочная диагональ
    Дано число n, n\le100n,n≤100. Создайте массив n\times n×n и заполните его по следующему правилу:
    числа на диагонали, идущей из правого верхнего в левый нижний угол, равны 1;
    числа, стоящие выше этой диагонали, равны 0
    числа, стоящие ниже этой диагонали, равны 2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine()); // число строк
+        int[,] a = new int[n, n]; // создание массива
+
+
+        for (int i = 0; i <n; i++)
+        {
+            for (int j = 0; j < n ; j++)
+            {
+                if (i == n - j - 1) a[i, j] = 1;
+                else if (i > n - j - 1) a[i, j] = 2;
+                Console.Write(a[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
